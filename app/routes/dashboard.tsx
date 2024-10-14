95461804dc54fb8aa3e21c5994b7ed3b498d23d1
Pavel Struhar <pstruhar@gmail.com>
2024-10-14 22:31:57 +0200

    Add meta

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,4 +1,12 @@
+import { type MetaFunction } from "@remix-run/cloudflare";
 import { Outlet } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "URL Shortener Dashboard" },
+    { name: "description", content: "URL Shortener Dashboard" },
+  ];
+};
 
 export default function Dashboard() {
   return (
